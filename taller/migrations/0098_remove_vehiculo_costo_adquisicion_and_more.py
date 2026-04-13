ba5d5f7f4e44ac4e15a46684e2bba3c11217158c
Mauricio Alvarado <tu_correo@ejemplo.com>
2026-04-12 21:53:53 -0400

    fix(migrations): remove duplicate AddField precio_compra

---
+++ taller/migrations/0098_remove_vehiculo_costo_adquisicion_and_more.py
@@ -0,0 +1,15 @@
+﻿from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('taller', '0097_safe_add_precio_compra'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='vehiculo',
+            name='costo_adquisicion',
+        ),
+    ]
