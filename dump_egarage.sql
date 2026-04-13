24f31a6dcd1262fbcf87812d583a478c5b7b0292
Mauricio Alvarado <mauricioatlanta@gmail.com>
2026-04-13 14:29:34 +0000

    backup antes de fix multi-tenant

---
+++ dump_egarage.sql
@@ -0,0 +1,26 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict XYuS23Osa7brF97YcxbhKme52GrEw7J9QTPK6eQRz3eTWtkSGgCRjnweDs4yYdV
+
+-- Dumped from database version 16.13 (Ubuntu 16.13-0ubuntu0.24.04.1)
+-- Dumped by pg_dump version 16.13 (Ubuntu 16.13-0ubuntu0.24.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict XYuS23Osa7brF97YcxbhKme52GrEw7J9QTPK6eQRz3eTWtkSGgCRjnweDs4yYdV
+
